9bc734346404c6f3b4a679b47df4ec76e960117f
Kevin Naserwan <kevinnaserwan@gmail.com>
2023-11-01 11:07:16 +0700

    verify

--- lib/pages/Auth/verification.dart
+++ lib/pages/Auth/verification.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:projectkomputasibergerak/pages/Auth/signin.dart';
+import 'package:projectkomputasibergerak/pages/Auth/signup.dart';
 
 class Verify extends StatefulWidget {
   const Verify({super.key});
@@ -73,6 +75,96 @@ class _VerifyState extends State<Verify> {
                           fontWeight: FontWeight.w400,
                           color: Color(0x80000000)),
                     ),
+                    SizedBox(
+                        height: 34.0), // Space between text and text fields
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        for (int i = 0; i < 4; i++)
+                          Container(
+                            width: 50.0,
+                            height: 50.0,
+                            decoration: BoxDecoration(
+                              color: Color(0xFFD9D9D9),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            margin: EdgeInsets.symmetric(horizontal: 12.0),
+                            child: TextField(
+                              textAlign: TextAlign.center,
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                  border: InputBorder.none, counterText: ""),
+                              textInputAction: TextInputAction.next,
+                              maxLength: 1,
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.w500,
+                                color: Color(0x80000000),
+                              ),
+                            ),
+                          ),
+                      ],
+                    ),
+                    SizedBox(height: 133.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const Signup(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        // ignore: deprecated_member_use
+                        primary: const Color(
+                            0xFF406882), // Ganti dengan warna yang diinginkan
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 142.0,
+                            vertical: 12.0), // Margin kiri-kanan 28px
+                      ),
+                      child: const Text(
+                        'Submit',
+                        style: TextStyle(
+                          fontSize: 16, // Ukuran font 16
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Didn’t received the code?',
+                          style: TextStyle(
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xFF000000),
+                          ),
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => const Signin()));
+                            },
+                            style: ButtonStyle(
+                              elevation: MaterialStateProperty.all(
+                                  0.0), // Menghilangkan shadowF
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.transparent),
+                            ),
+                            child: Text(
+                              'Resend',
+                              style: TextStyle(
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xFF6998AB),
+                                  decoration: TextDecoration.underline),
+                            )),
+                      ],
+                    )
                   ]),
             )
           ],
